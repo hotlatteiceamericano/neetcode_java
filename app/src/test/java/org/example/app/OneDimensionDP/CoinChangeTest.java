4af74fc3308c5e1e5b4ebb43bf795108676d3d36
hotlatteiceamericano <hotlatteiceamericano@gmail.com>
2026-05-05 12:53:41 +0800

    DecodeWays and CoinChange
    1. it asks for how many coins have been used, hence when remains is 0,
    there will be 0 coins being used.
    2. different from Decode Ways, which asks for "number of combinations",
    that when index exceeds the input string, it has 0 combination

--- app/src/test/java/org/example/app/OneDimensionDP/CoinChangeTest.java
+++ app/src/test/java/org/example/app/OneDimensionDP/CoinChangeTest.java
@@ -1,47 +1,58 @@
 package org.example.app.OneDimensionDP;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 class CoinChangeSolution {
+    static int INVALID = Integer.MAX_VALUE;
+
     public int coinChange(int[] coins, int amount) {
-        Map<Integer, Integer> cache = new HashMap<>();
-
-        int res = dfs(coins, amount, amount, cache);
-
+        int res = dfs(coins, amount, amount, new HashMap<>());
         return res == Integer.MAX_VALUE ? -1 : res;
     }
 
-    int dfs(int[] coins, int amount, int curr, Map<Integer, Integer> cache) {
-        if (curr == 0) return 0;
-        if (curr < 0) return Integer.MAX_VALUE;
+    // asks for number of coins, but not combination
+    // need to keep track of how many coins have been used so far
+    // also need to keep track of what is the remaining amount
 
-        if (cache.containsKey(curr)) return cache.get(curr);
-
-        int minCount = Integer.MAX_VALUE;
-        for (int i=0; i<coins.length; i++) {
-            int count = dfs(coins, amount, curr-coins[i], cache);
-            if (count != Integer.MAX_VALUE)
-                minCount = Math.min(minCount, count);
+    // what I didn't understand is, it is how many coins needed to complete the remains
+    // however passing numCoins from top means: "how many coins have spent"
+    // in DP DFS, I need to think when ends, what is the baseline case
+    int dfs(int[] coins, int amount, int remains, Map<Integer, Integer> cache) {
+        if (remains == 0) {
+            return 0;
+        }
+        if (remains < 0) {
+            return INVALID;
         }
 
-        if (minCount != Integer.MAX_VALUE){
-            cache.put(curr, minCount+1);
-        } else {
-            cache.put(curr, Integer.MAX_VALUE);
+        if (cache.containsKey(remains)) {
+            return cache.get(remains);
         }
 
-        return cache.get(curr);
+        int minCoins = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int res = dfs(coins, amount, remains - coin, cache);
+            if (res != INVALID) {
+                minCoins = Math.min(minCoins, res + 1);
+            }
+        }
 
+        cache.put(remains, minCoins);
+
+        return cache.get(remains);
     }
 }
 
 public class CoinChangeTest {
     @Test
     void test() {
-        new CoinChangeSolution().coinChange(new int[]{357,239,73,52}, 9832);
+        // assertEquals(35, new CoinChangeSolution().coinChange(new int[] {357, 239, 73, 52},
+        // 9832));
+        assertEquals(3, new CoinChangeSolution().coinChange(new int[] {1, 2, 5}, 11));
     }
 }
--- app/src/test/java/org/example/app/OneDimensionDP/DecodeWaysTest.java
+++ app/src/test/java/org/example/app/OneDimensionDP/DecodeWaysTest.java
@@ -3,24 +3,39 @@ package org.example.app.OneDimensionDP;
 import org.junit.jupiter.api.Test;
 
 class DecodeWaysSolution {
-    private int dfs(int i, String s) {
-        if (i == s.length())
+	  public int numDecodings(String s) {
+        // 11111x
+        // dfs when possible combination
+        // when at the end, means one valid deocde way
+        return dfs(s, 0, new HashMap<>());
+    }
+
+    int dfs(String s, int i, Map<Integer, Integer> cache) {
+        if (i >= s.length()) {
             return 1;
-        if (s.charAt(i) == '0')
-            return 0;
-
-        int res = dfs(i + 1, s);
-        if (i < s.length() - 1) {
-            if (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) < '7')) {
-                res += dfs(i + 2, s);
-            }
         }
-        return res;
-    }
 
-    public int numDecodings(String s) {
-        return dfs(0, s);
-    }
+        if (cache.containsKey(i)) {
+            return cache.get(i);
+        }
+
+        // can either decide one number or decode two numbers
+        // decoding one number only has one condition on 0
+        if (s.charAt(i) == '0') {
+            return 0;
+        }
+
+        int waysDecode = dfs(s, i+1, cache);
+        if (i < s.length()-1 && (
+            s.charAt(i) == '1' ||
+            (s.charAt(i) == '2' && Character.getNumericValue(s.charAt(i+1)) < 7))) {
+                waysDecode += dfs(s, i+2, cache);
+        }
+
+        cache.put(i, waysDecode);
+
+        return cache.get(i);
+
 }
 
 public class DecodeWaysTest {
